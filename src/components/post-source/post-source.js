847a2487ae8d2f1de3c35536bee6efd2b54a334a
Michelle Barker <michellebarker@michelles-mbp-2.home>
2018-06-01 23:19:10 +0100

    Fixing JS error

--- src/components/post-source/post-source.js
+++ src/components/post-source/post-source.js
@@ -3,7 +3,7 @@ import styles from './post-source.module.scss'
 
 export const SourceDetails = props => {
 	const hasSrc = props.url
-	if (hasSrc) {
+	if (hasSrc !== false) {
 		return (
 			<p className={styles.src}>
 				<a href={props.url}>{props.text}</a>
