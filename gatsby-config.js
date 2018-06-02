afa5ebf4e0cfc5ebd1ff648de9084ee315f80f4e
Michelle Barker <michellebarker@michelles-mbp-2.home>
2018-06-02 12:39:28 +0100

    Head

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,63 +14,11 @@ module.exports = {
 		`gatsby-plugin-glamor`,
 		`gatsby-transformer-remark`,
 		`gatsby-plugin-sass`,
+		`gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-plugin-typography`,
 			options: {
 				pathToConfigModule: `src/utils/typography`
-			}
-		},
-		{
-			resolve: `gatsby-plugin-feed`,
-			options: {
-				query: `
-					{
-						site {
-							siteMetadata {
-								title
-								description
-								siteUrl
-								site_url: siteUrl
-							}
-						}
-					}
-				`,
-				feeds: [
-					{
-						serialize: ({ query: { site, allMarkdownRemark } }) => {
-							return allMarkdownRemark.edges.map(edge => {
-								return Object.assign({}, edge.node.frontmatter, {
-									description: edge.node.excerpt,
-									url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-									guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-									custom_elements: [{ 'content:encoded': edge.node.html }]
-								})
-							})
-						},
-						query: `
-							{
-								allMarkdownRemark(
-									limit: 1000,
-									sort: { order: DESC, fields: [frontmatter___date] },
-									filter: {frontmatter: { draft: { ne: true } }}
-								) {
-									edges {
-										node {
-											excerpt
-											html
-											fields { slug }
-											frontmatter {
-												title
-												date
-											}
-										}
-									}
-								}
-							}
-						`,
-						output: '/rss.xml'
-					}
-				]
 			}
 		}
 	]
