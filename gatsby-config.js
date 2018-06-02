41d6ce439185a8b12f9200db59bce6528a8fd376
Michelle Barker <michellebarker@michelles-mbp-2.home>
2018-06-02 12:09:09 +0100

    404 page and RSS plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
 				path: `${__dirname}/src/`
 			}
 		},
-		// {
-		// 	resolve: `gatsby-plugin-postcss-sass`,
-		// 	options: {
-		// 		postCssPlugins: [],
-		// 		precision: 8 // SASS default: 5
-		// 	}
-		// },
 		`gatsby-plugin-glamor`,
 		`gatsby-transformer-remark`,
 		`gatsby-plugin-sass`,
@@ -25,6 +18,59 @@ module.exports = {
 			resolve: `gatsby-plugin-typography`,
 			options: {
 				pathToConfigModule: `src/utils/typography`
+			}
+		},
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
+					{
+						site {
+							siteMetadata {
+								title
+								description
+								siteUrl
+								site_url: siteUrl
+							}
+						}
+					}
+				`,
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark } }) => {
+							return allMarkdownRemark.edges.map(edge => {
+								return Object.assign({}, edge.node.frontmatter, {
+									description: edge.node.excerpt,
+									url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+									guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+									custom_elements: [{ 'content:encoded': edge.node.html }]
+								})
+							})
+						},
+						query: `
+							{
+								allMarkdownRemark(
+									limit: 1000,
+									sort: { order: DESC, fields: [frontmatter___date] },
+									filter: {frontmatter: { draft: { ne: true } }}
+								) {
+									edges {
+										node {
+											excerpt
+											html
+											fields { slug }
+											frontmatter {
+												title
+												date
+											}
+										}
+									}
+								}
+							}
+						`,
+						output: '/rss.xml'
+					}
+				]
 			}
 		}
 	]
---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styles from '../layouts/styles.module.scss'
+import layout from '../layouts/layout.module.scss'
+import globals from '../globals/globals.module.scss'
+
+export default ({ data }) => (
+	<div className={layout.postContent}>
+		<h1>404</h1>
+		<p>
+			Uh-oh, looks like the page you’re looking for doesn’t exist. Sorry about
+			that.
+		</p>
+		<Link to={`/`} className={styles.link}>
+			<span className={globals.hl}>Return to homepage</span>
+		</Link>
+	</div>
+)
