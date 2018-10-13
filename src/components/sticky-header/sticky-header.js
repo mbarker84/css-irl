64b95b9a826cd403a50725d6011f65c95950c1b3
Michelle Barker <michellebarker@michelles-mbp-2.home>
2018-10-13 13:55:14 +0100

    Sticky header

--- src/components/sticky-header/sticky-header.js
+++ src/components/sticky-header/sticky-header.js
@@ -12,17 +12,22 @@ export const StickyHeader = () => {
 				pinStart={100}
 				disableInlineStyles
 			>
-				<Link className={styles.link} to={`/`}>
-					<h1 className={styles.siteTitle}>
-						<span className={styles.mainTitle}>CSS</span>
-						<span className={styles.subheading}>
-							{`{`}
-							In Real Life
-							{`}`}
-						</span>
-					</h1>
+				<Link className={styles.homeLink} to={`/`}>
+					<h3>
+						CSS {`{`} IRL {`}`}
+					</h3>
 				</Link>
 			</Headroom>
+			<Link className={styles.link} to={`/`}>
+				<h1 className={styles.siteTitle}>
+					<span className={styles.mainTitle}>CSS</span>
+					<span className={styles.subheading}>
+						{`{`}
+						In Real Life
+						{`}`}
+					</span>
+				</h1>
+			</Link>
 		</header>
 	)
 }
