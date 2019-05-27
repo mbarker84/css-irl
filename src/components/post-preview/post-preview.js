a7d026805496208f51be2dc32f6c1105f2866055
Michelle Barker <mbarker_84@hotmail.co.uk>
2019-05-27 22:35:56 +0100

    Adding contents from index.js to listing-page.js

--- src/components/post-preview/post-preview.js
+++ src/components/post-preview/post-preview.js
@@ -11,32 +11,7 @@ export const PostPreview = props => {
 		}
 	}
 
-	if (externalLink) {
-		return (
-			<a
-				href={props.externalLink}
-				style={{ textDecoration: `none`, color: `inherit` }}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<div className={styles.postPreview}>
-					{seriesTitle()}
-					<h2 className={styles.title}>
-						<span className={globals.hl}>{props.title}</span>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.2 12.2">
-							<path d="M5.7 0v1.5h4L4.5 6.7l1 1.1 5.3-5.2v3.9h1.4V0z" />
-							<path fill="none" d="M3.4 6.7l3-2.9H1.5v7h7V5.9l-3 2.9z" />
-							<path d="M8.5 5.9v4.9h-7v-7h4.9l1.5-1.6H0v10h10V4.4z" />
-						</svg>
-					</h2>
-					<time className={styles.date} dateTime={props.date}>
-						{props.date}
-					</time>
-					<div className={styles.content}>{props.excerpt}</div>
-				</div>
-			</a>
-		)
-	} else {
+	if (!externalLink) {
 		return (
 			<Link to={props.to} style={{ textDecoration: `none`, color: `inherit` }}>
 				<div className={styles.postPreview}>
@@ -52,4 +27,29 @@ export const PostPreview = props => {
 			</Link>
 		)
 	}
+
+	return (
+		<a
+			href={props.externalLink}
+			style={{ textDecoration: `none`, color: `inherit` }}
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<div className={styles.postPreview}>
+				{seriesTitle()}
+				<h2 className={styles.title}>
+					<span className={globals.hl}>{props.title}</span>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.2 12.2">
+						<path d="M5.7 0v1.5h4L4.5 6.7l1 1.1 5.3-5.2v3.9h1.4V0z" />
+						<path fill="none" d="M3.4 6.7l3-2.9H1.5v7h7V5.9l-3 2.9z" />
+						<path d="M8.5 5.9v4.9h-7v-7h4.9l1.5-1.6H0v10h10V4.4z" />
+					</svg>
+				</h2>
+				<time className={styles.date} dateTime={props.date}>
+					{props.date}
+				</time>
+				<div className={styles.content}>{props.excerpt}</div>
+			</div>
+		</a>
+	)
 }
--- src/templates/listing-page.js
+++ src/templates/listing-page.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import globals from '../globals/globals.module.scss'
+import layout from '../layouts/layout.module.scss'
 
-export default () => {
+import { PostPreview } from '../components/post-preview/post-preview'
+import styles from './listing-page.module.scss'
+
+export default props => {
+	const { data } = props
+	const { currentPage, numPages } = props.pageContext
+	const isFirst = currentPage === 1
+	const isLast = currentPage === numPages
+	const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+	const nextPage = (currentPage + 1).toString()
+
 	return (
 		<Layout>
-			<h1>Page</h1>
+			<div>
+				<h4 className={globals.postsCount}>
+					{data.allMarkdownRemark.totalCount} Posts
+				</h4>
+				<ul className={layout.postsGrid}>
+					{data.allMarkdownRemark.edges.map(({ node }) => (
+						<li key={node.id}>
+							<PostPreview
+								to={node.fields.slug}
+								title={node.frontmatter.title}
+								excerpt={node.excerpt}
+								date={node.frontmatter.date}
+								externalLink={node.frontmatter.externalLink}
+								series={node.frontmatter.series}
+							/>
+						</li>
+					))}
+				</ul>
+			</div>
+			<nav className={styles.pagination}>
+				{!isFirst && (
+					<Link to={prevPage} rel="prev">
+						← Previous Page
+					</Link>
+				)}
+				{!isLast && (
+					<Link to={nextPage} rel="next">
+						Next Page →
+					</Link>
+				)}
+			</nav>
 		</Layout>
 	)
 }
@@ -17,12 +59,20 @@ export const pageQuery = graphql`
 			limit: $limit
 			skip: $skip
 		) {
+			totalCount
 			edges {
 				node {
 					excerpt
+					fields {
+						slug
+					}
 					frontmatter {
 						date(formatString: "DD MMMM, YYYY")
 						title
+						title
+						date(formatString: "DD MMMM, YYYY")
+						externalLink
+						series
 					}
 				}
 			}
