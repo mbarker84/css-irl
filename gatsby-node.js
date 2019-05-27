c1cfcc0c8dffc9bbd79c55e158e02bd4a2da2efb
Michelle Barker <mbarker_84@hotmail.co.uk>
2019-05-27 23:07:40 +0100

    Fixing homepage error

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,7 +51,7 @@ exports.createPages = ({ graphql, actions }) => {
 				Array.from({ length: numPages }).forEach((_, i) => {
 					createPage({
 						path: i === 0 ? `/` : `/${i + 1}`,
-						component: path.resolve('./src/templates/blog-list.js'),
+						component: path.resolve('./src/templates/listing-page.js'),
 						context: {
 							limit: postsPerPage,
 							skip: i * postsPerPage,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,39 @@
 import React from 'react'
-import ListingPage from '../templates/listing-page'
 
-export default () => {
-	return <ListingPage />
+import Layout from '../components/layout'
+import globals from '../globals/globals.module.scss'
+import layout from '../layouts/layout.module.scss'
+
+import { PostPreview } from '../components/post-preview/post-preview'
+
+export default ({ data }) => {
+	return <Layout />
 }
+
+export const query = graphql`
+	query IndexQuery {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						date(formatString: "DD MMMM, YYYY")
+						externalLink
+						series
+					}
+					fields {
+						slug
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`
