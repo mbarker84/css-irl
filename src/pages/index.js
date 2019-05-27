b313dbb117e070152b7723d727c60cf8e9813dfc
Michelle Barker <mbarker_84@hotmail.co.uk>
2019-05-27 23:13:15 +0100

    Fixing homepage more

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,29 @@ import layout from '../layouts/layout.module.scss'
 import { PostPreview } from '../components/post-preview/post-preview'
 
 export default ({ data }) => {
-	return <Layout />
+	return (
+		<Layout>
+			<div>
+				<h4 className={globals.postsCount}>
+					{data.allMarkdownRemark.totalCount} Posts
+				</h4>
+				<ul className={layout.postsGrid}>
+					{data.allMarkdownRemark.edges.map(({ node }) => (
+						<li key={node.id}>
+							<PostPreview
+								to={node.fields.slug}
+								title={node.frontmatter.title}
+								excerpt={node.excerpt}
+								date={node.frontmatter.date}
+								externalLink={node.frontmatter.externalLink}
+								series={node.frontmatter.series}
+							/>
+						</li>
+					))}
+				</ul>
+			</div>
+		</Layout>
+	)
 }
 
 export const query = graphql`
