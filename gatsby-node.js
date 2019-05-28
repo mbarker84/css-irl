93494d9eec8e3f4ca193095940b77d09c35a96b9
Michelle Barker <mbarker_84@hotmail.co.uk>
2019-05-28 22:22:12 +0100

    Create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,13 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const _ = require('lodash')
 
 exports.createPages = ({ graphql, actions }) => {
 	const { createPage } = actions
+	const blogListingTemplate = path.resolve('src/templates/index.js')
+	const blogPostTemplate = path.resolve('src/templates/blog-post.js')
+	const tagTemplate = path.resolve('src/templates/tags.js')
+
 	return new Promise((resolve, reject) => {
 		graphql(`
 			{
@@ -12,12 +17,17 @@ exports.createPages = ({ graphql, actions }) => {
 							fields {
 								slug
 							}
+							frontmatter {
+								tags
+							}
 						}
 					}
 				}
 			}
 		`).then(result => {
 			const posts = result.data.allMarkdownRemark.edges
+
+			// Post detail pages
 			posts.forEach((post, index) => {
 				const previous =
 					index === posts.length - 1 ? null : posts[index + 1].node
@@ -25,7 +35,7 @@ exports.createPages = ({ graphql, actions }) => {
 
 				createPage({
 					path: post.node.fields.slug,
-					component: path.resolve(`./src/templates/blog-post.js`),
+					component: blogPostTemplate,
 					context: {
 						slug: post.node.fields.slug,
 						previous,
@@ -36,15 +46,35 @@ exports.createPages = ({ graphql, actions }) => {
 				const postsPerPage = 6
 				const numPages = Math.ceil(posts.length / postsPerPage)
 
+				// Listing page (index)
 				Array.from({ length: numPages }).forEach((_, i) => {
 					createPage({
 						path: i === 0 ? `/` : `/${i + 1}`,
-						component: path.resolve('./src/templates/index.js'),
+						component: blogListingTemplate,
 						context: {
 							limit: postsPerPage,
 							skip: i * postsPerPage,
 							numPages,
 							currentPage: i + 1
+						}
+					})
+				})
+
+				// Tag pages:
+				let tags = []
+				_.each(posts, edge => {
+					if (_.get(edge, 'node.frontmatter.tags')) {
+						tags = tags.concat(edge.node.frontmatter.tags)
+					}
+				})
+				tags = _.uniq(tags)
+
+				tags.forEach(tag => {
+					createPage({
+						path: `/tags/${_.kebabCase(tag)}/`,
+						component: tagTemplate,
+						context: {
+							tag
 						}
 					})
 				})
