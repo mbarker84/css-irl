1416cac6848f749772a9903ffc6c770007722b4e
Michelle Barker <mbarker_84@hotmail.co.uk>
2019-05-31 21:51:26 +0100

    Upgrading React

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ]
+	globals: {
+		__PATH_PREFIX__: true
+	},
+	parser: 'babel-eslint',
+	extends: ['standard', 'plugin:react/recommended'],
+	rules: {
+		'comma-dangle': ['error', 'always-multiline']
+	}
 }
