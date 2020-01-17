b38e52bd100b1253e4181ce24b19c2bad9bb2264
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-01-17 21:33:24 +0000

    Add dangerouslySetInnerHTML for ad script tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -32,8 +32,7 @@ const renderExternalLink = srcUrl => {
 
 const img = `https://css-irl.info${metaImg}`;
 
-export default ({ data, location, pathContext }) => {
-  console.log(pathContext);
+export default ({ data, location, pathContext, dangerouslySetInnerHTML }) => {
   const post = data.markdownRemark;
   const { series, srcUrl, source, title, tags, date } = post.frontmatter;
   const { next, previous } = pathContext;
@@ -73,16 +72,16 @@ export default ({ data, location, pathContext }) => {
             className={styles.richtext}
           />
           {renderExternalLink(srcUrl)}
-          <div>
-            {
-              <script
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<script
                 async
                 type="text/javascript"
                 src="//cdn.carbonads.com/carbon.js?serve=CE7D62J7&placement=css-irlinfo"
                 id="_carbonads_js"
-              ></script>
-            }
-          </div>
+              ></script>`
+            }}
+          />
           <PostPaginate previous={previous} next={next}></PostPaginate>
         </div>
       </div>
