034aa1bbac3177f189a1d6cd83c4712e7de585aa
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-01-26 16:41:21 +0000

    Remove Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,22 +116,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-150229629-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0
-      }
-    },
-    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`exo\:400,800`, `pt serif\:400,400i`],
