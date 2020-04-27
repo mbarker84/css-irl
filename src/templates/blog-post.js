fcd5c273e07e857482ae6b99198943904c57c005
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-04-27 22:31:44 +0100

    Revert script

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -75,12 +75,7 @@ export default ({ data, location, pageContext }) => {
           <div className={styles.adBlock}>
             <div
               dangerouslySetInnerHTML={{
-                __html: `<script
-                  async
-                  type="text/javascript"
-                  src="cdn.carbonads.com/carbon.js?serve=CE7D62J7&placement=css-irlinfo"
-                  id="_carbonads_js"
-                ></script>`,
+                __html: `<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CE7D62J7&placement=css-irlinfo" id="_carbonads_js"></script>`,
               }}
             />
           </div>
