5bcc6bc193a7c35cb2b3a6e3c7d14d9dd7086806
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-05-01 08:46:33 +0100

    Revert "Remove twitter plugin dure to failed builds"
    This reverts commit 1c289c5f3d89c7709f127b642cfcecddcd5de856.

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-twitter`,
     {
       resolve: "gatsby-remark-video",
       options: {
