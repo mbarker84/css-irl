1bdea970320d78b0b1dae9b0615833da7f439afe
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-05-01 22:04:27 +0100

    Remove ad block

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -72,13 +72,6 @@ export default ({ data, location, pageContext }) => {
             className={styles.richtext}
           />
           {renderExternalLink(srcUrl)}
-          <div className={styles.adBlock}>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: `<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CE7D62J7&placement=css-irlinfo" id="_carbonads_js"></script>`,
-              }}
-            />
-          </div>
           <PostPaginate previous={previous} next={next}></PostPaginate>
         </div>
       </div>
